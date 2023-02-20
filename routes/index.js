e14bfb3b64095a5185dfcb97af7821446aa47d36
Mauricio Brauer <mauriciobrauer@gmail.com>
2023-02-20 15:58:04 -0600

    Commit 4

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var express = require("express");
 var router = express.Router();
 
 const configuration = new Configuration({
-  //apiKey: "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",
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
@@ -18,21 +17,21 @@ router.get("/", function (req, res, next) {
 
 router.post("/peticion", async (req, res) => {
   const { tipo_contrato } = req.body;
-  console.log(configuration.apiKey);
-  /*const response = await openai.createCompletion({
+  //console.log(configuration.apiKey);
+  const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `Pretende que eres un abogado, escribe un contrato de ${tipo_contrato}`,
     max_tokens: 1000,
     temperature: 0,
   });
   //res.send("ok");
-  
+
   console.log(response.data);
   if (response.data) {
     if (response.data.choices) {
       res.send(response.data.choices[0].text);
     }
-  }*/
+  }
 });
 
 module.exports = router;
